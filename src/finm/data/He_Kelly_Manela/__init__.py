69b918e4555e044dcb2c14b8c6c61f9dad8f8197
Jared Szajkowski <jared.szajkowski@gmail.com>
2025-12-25 21:33:23 -0600

    Rename He_Kelly_Manela

--- src/finm/data/He_Kelly_Manela/__init__.py
+++ src/finm/data/He_Kelly_Manela/__init__.py
@@ -4,7 +4,7 @@ Data module - He, Kelly, and Manela
 This module provides functions for pulling, loading, and cleaning data.
 """
 
-from finm.data.he_kelly_manela.pull_he_kelly_manela import (
+from finm.data.He_Kelly_Manela.pull_he_kelly_manela import (
     pull_he_kelly_manela,
     load_he_kelly_manela_factors_monthly,
     load_he_kelly_manela_factors_daily,
